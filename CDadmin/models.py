deb21c0bdb289d62062a27b1ae76fbb130d6f5da
vden <vden@hmgaz.ru>
2017-04-08 15:59:51 +0500

    08042017

--- CDadmin/models.py
+++ CDadmin/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
-
+class Account(models.Model):
+    cd_account_short_title = models.CharField('short title',max_length=50)
+    cd_account_title = models.CharField('title', max_length=125)
 
 
